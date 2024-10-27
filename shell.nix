3d6a5f743dfbfb17113aa24aee97553d2d75af26
jole84 <johanlennartsson84@gmail.com>
2024-10-27 08:49:14 +0100

    update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,13 +1,24 @@
+# example shell.nix file, copy to dir, select relevant packages and run nix-shell
+
 let
   pkgs = import <nixpkgs> { config = { allowUnfree = true; }; overlays = []; };
 in
 
 pkgs.mkShellNoCC {
   packages = with pkgs; [
+    # select packages availible in the shell here
     fastfetch
+    
+    (python3.withPackages (python-pkgs: with python-pkgs; [
+      # select packages availible in Python here
+      pandas
+      requests
+    ]))
   ];
-  # shellHook =
-  #   ''
-  #     fish
-  #   '';
+
+  # commands to run when nix-shell is started
+  shellHook =
+    ''
+      echo "nix-shell started!"
+    '';
 }
