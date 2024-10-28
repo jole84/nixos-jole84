e2eaae93b4eada83872c910c06d5b159de25841e
jole84 <77931011+jole84@users.noreply.github.com>
2024-10-28 21:44:36 +0100

    disable x11

--- configuration.nix
+++ configuration.nix
@@ -70,7 +70,7 @@
 
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
+  # services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
